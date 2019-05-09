c9d7dcbd4afcf4fb3e3969c6e21b99bbb837e9be
tmushayahama <tmushayahama@gmail.com>
2019-05-09 11:47:46 -0700

    added orderBy base clause

---
+++ src/models/clause/orderBy.ts
@@ -0,0 +1,23 @@
+import { Clause } from './../clause';
+
+export type Direction = 'DESC'
+    | 'desc'
+    | 'ASC'
+    | 'asc'
+    | null
+    | undefined;
+
+export class OrderBy extends Clause {
+    private _fields: string
+    private _direction: Direction = 'desc';
+
+    constructor(fields: string, direction?: Direction) {
+        super();
+        this._fields = fields;
+        this._direction = direction;
+    }
+
+    build() {
+        return `${this._direction} (${this._fields})`;
+    }
+}
